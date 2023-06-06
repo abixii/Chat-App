611d2a96be6fb028dd92f68af996868c1cf3ba15
Abhimanue T J <abhimanuetj12@gmail.com>
2023-06-06 21:47:38 +0530

    added firebase and initalized

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDWpPvrHcKxPCU0JiXi7IHcF2P3MxbiktU',
+  authDomain: 'chat-web-app-2bda2.firebaseapp.com',
+  projectId: 'chat-web-app-2bda2',
+  storageBucket: 'chat-web-app-2bda2.appspot.com',
+  messagingSenderId: '902451383729',
+  appId: '1:902451383729:web:b5b64eab0f39e5c4df0190',
+  measurementId: 'G-1K43M6EZR7',
+};
+
+const app = firebase.initializeApp(config);
